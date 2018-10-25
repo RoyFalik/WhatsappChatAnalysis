14c5d4ac7eecd668cf70740fa4ae7c7025c5c36e
Roy Falik <roy.falik@utexas.edu>
2018-10-24 22:50:28 -0500

    Added wrapper to keep data persistent

---
+++ wrapper.py
@@ -0,0 +1,13 @@
+import sys
+import analyze
+import importlib
+
+part1Cache = None
+if __name__ == "__main__":
+    while True:
+        if not part1Cache:
+            part1Cache = analyze.parse()
+        analyze.analyze(part1Cache)
+        print("Press enter to re-run the script, CTRL-C to exit")
+        sys.stdin.readline()
+        importlib.reload(analyze)
