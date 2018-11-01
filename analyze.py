f8190cae65154601808e264a1010afdd4a229317
Roy Falik <roy.falik@utexas.edu>
2018-10-31 19:10:57 -0500

    Added analysis of various metrics

--- analyze.py
+++ analyze.py
@@ -1,6 +1,13 @@
 import sys
 from datetime import datetime
 from functools import reduce
+import pandas as pd
+from collections import Counter
+import string
+import difflib
+import seaborn as sns
+import matplotlib.pyplot as plt
+import emoji
 
 
 class Message:
@@ -24,19 +31,28 @@ TotalMessageCount = 0
 TotalAnalyzedLinesCount = 0
 MessageList = []
 
+PunctuationTable = str.maketrans(dict.fromkeys(string.punctuation+"’"))
+
+# todo:
+# count audio messages
+# count image messages
+# most used words per person
+
 
 def sanitizeToMessage(line) -> list:
     endOfTimeStamp = line.find(']')
     if (endOfTimeStamp == -1):
         print("Fatal error: End of Timestamp not found in line:", line)
-        quit()
+        raise ValueError(
+            "Fatal error: End of Timestamp not found in line:" + line)
 
     endOfSender = line.find(':', endOfTimeStamp+1)
     if (endOfSender == -1):
         print("Fatal error: End of Sender not found in line:", line)
-        quit()
+        raise ValueError(
+            "Fatal error: End of Sender not found in line:" + line)
 
-    return line[endOfSender+1:].split()
+    return line[endOfSender+1:].lower().translate(PunctuationTable).split()
 
 
 def getDateSent(line) -> datetime:
@@ -57,12 +73,14 @@ def getSender(line) -> str:
     endOfTimeStamp = line.find(']')
     if (endOfTimeStamp == -1):
         print("Fatal error: End of Timestamp not found in line:", line)
-        quit()
+        raise ValueError(
+            "Fatal error: End of Timestamp not found in line:" + line)
 
     endOfSender = line.find(':', endOfTimeStamp+1)
     if (endOfSender == -1):
         print("Fatal error: End of Sender not found in line:", line)
-        quit()
+        raise ValueError(
+            "Fatal error: End of Sender not found in line:" + line)
 
     return line[endOfTimeStamp+2:endOfSender]
 
@@ -108,22 +126,102 @@ def read_data(filename):
             TotalAnalyzedLinesCount += 1
 
 
-print('Looking for WhatsApp File: ', str(sys.argv[1]))
-read_data(sys.argv[1])
-print('Message count: ', len(MessageList))
+def parse(filename):
+    fp = filename
+    print('Looking for WhatsApp File: ', fp)
+    read_data(fp)
+    print('Message count: ', len(MessageList))
+    return MessageList
 
-RoyMessage = list(filter(lambda message: message.sender == 'Roy', MessageList))
-print(len(RoyMessage))
 
-GonieMessage = list(filter(lambda message: message.sender == 'Gonie  Altman', MessageList))
-print(len(GonieMessage))
+def analyze(messageList):
+    print("------------STATISTICS-------------\n\n\n")
 
-RoyWordCount = list(map(lambda message: len(message.text), RoyMessage))
-RoyWordCountTotal = reduce((lambda x, y: x + y), RoyWordCount)
+    wordsDf = pd.DataFrame(data=list(map(lambda message: [message.sender, len(message.text)], messageList)), columns=[
+                           "Sender", "Number of Words"]).groupby("Sender").agg({'Number of Words': 'sum'}).reset_index()
 
-GonieWordCount = list(map(lambda message: len(message.text), GonieMessage))
-GonieWordCountTotal = reduce((lambda x, y: x + y), GonieWordCount)
+    messagesPerPersonDF = pd.DataFrame(data=list(map(lambda message: [message.sender, 1], messageList)), columns=[
+        "Sender", "Number of Messages"]).groupby("Sender").agg({'Number of Messages': 'sum'}).reset_index()
 
-print(RoyWordCountTotal)
-print(GonieWordCountTotal)
+    numberOfMessagesPerDayDF = pd.DataFrame(data=list(map(lambda message: [message.dateSent.date(), 1], messageList)), columns=[
+        "Date Sent", "Number of Messages"]).groupby("Date Sent").agg({'Number of Messages': 'sum'}).reset_index()
 
+    numberOfMessagesPerDayOfWeekDF = pd.DataFrame(data=list(map(lambda message: [message.sender, message.dateSent.date().strftime("%A"), 1], messageList)), columns=[
+        "Sender", "Day of Week", "Number of Messages"]).groupby(["Day of Week", "Sender"]).agg({'Number of Messages': 'sum'}).reset_index()
+
+    numberOfMessagesPerHourOfDayDF = pd.DataFrame(data=list(map(lambda message: [message.sender.split()[:1][0], message.dateSent.strftime("%H"), 1], messageList)), columns=[
+        "Sender", "Hour of Day", "Number of Messages"]).groupby(["Hour of Day", "Sender"]).agg({'Number of Messages': 'sum'}).reset_index()
+
+    # aggregate word count
+    WordsDictionary = Counter()
+    for message in messageList:
+        for word in message.text:
+            WordsDictionary[word] += 1
+
+    with open(r"/mnt/d/Projects/ChatAnalysis/EnglishStopwords.txt") as f:
+        for line in f:
+            WordsDictionary[line.lower().rstrip(
+            ).translate(PunctuationTable)] = 0
+    wordCount = pd.DataFrame(
+        data=WordsDictionary.most_common(15), columns=["Word", "Count"])
+
+    # aggregate emoji count
+    EmojiDictionary = Counter()
+    for message in messageList:
+        for word in message.text:
+            if word in emoji.UNICODE_EMOJI:
+                EmojiDictionary[word] += 1
+
+    emojiCount = pd.DataFrame(
+        data=EmojiDictionary.most_common(15), columns=["Emoji", "Count"])
+
+    f = open(
+        r"MessagesOverTime_days.txt", "w")
+    f.write(numberOfMessagesPerDayDF.to_string())
+
+    f = open(
+        r"MessagesOverTime_weekdays.txt", "w")
+    f.write(numberOfMessagesPerDayOfWeekDF.to_string())
+
+    f = open(
+        r"MessagesOverTime_hoursofday.txt", "w")
+    f.write(numberOfMessagesPerHourOfDayDF.to_string())
+
+    f = open(
+        r"MessagesOverTime_hoursofday.txt", "w")
+    f.write(numberOfMessagesPerHourOfDayDF.to_string())
+
+    f = open(
+        r"MostUsedWords.txt", "w")
+    f.write(wordCount.to_string())
+
+    f = open(
+        r"MostUsedEmojis.txt", "w")
+    f.write(emojiCount.to_string())
+
+    # printing results
+    # print("---Most Used Words---")
+    # print(wordCount)
+    # print("\n\n\n")
+
+    # print("---Highest Volume Days---")
+    # print(numberOfMessagesPerDayDF)
+    # print("\n\n\n")
+
+    # print("---Words Sent Per Person---")
+    # print(wordsDf)
+    # print("\n\n\n")
+
+    # print("---Messages Sent By Weekday---")
+    # print(numberOfMessagesPerDayOfWeekDF)
+    # print("\n\n\n")
+
+    # print("---Messages Sent By Hour of Day---")
+    # print(numberOfMessagesPerHourOfDayDF)
+    # print("\n\n\n")
+
+    # sns.set(style="whitegrid")
+    # #sns.set_color_codes("Spectral")
+    # sns.barplot(x="Sender", y="Number of Messages", data=messagesPerPersonDF, palette='Spectral')
+
+    # plt.show()
--- wrapper.py
+++ wrapper.py
@@ -6,7 +6,7 @@ part1Cache = None
 if __name__ == "__main__":
     while True:
         if not part1Cache:
-            part1Cache = analyze.parse()
+            part1Cache = analyze.parse(sys.argv[1])
         analyze.analyze(part1Cache)
         print("Press enter to re-run the script, CTRL-C to exit")
         sys.stdin.readline()
